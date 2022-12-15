962df12123e5ce964cfa1a5728058662bc162e41
Elly Loel <hello@ellyloel.com>
2022-12-15 16:06:04 +1100

    ✨ feat: updating hero component to consume design tokens

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '5335e1d8cf8020710fa8');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '14a41c417e8067ffa8d5');
